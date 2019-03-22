c6c482d502c011582c1c2a574edf6e20998cfd69
Maxime CHAREYRON <maxime.chareyron@enseirb-matmeca.fr>
2019-03-22 17:53:08 +0100

    create basic files

--- src/Client/src/mainWindow/ClientController.java
+++ src/Client/src/mainWindow/ClientController.java
@@ -1,5 +1,45 @@
 package mainWindow;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
 public class ClientController {
+    public static final String CONFIG_FILE = "./path/to/config.cfg";
+
+    private Properties config = new Properties();
+    private SocketHandler s;
+
+    private ArrayList<Fish> fishArrayList = new ArrayList<Fish>();
+
+
+    public ClientController() throws IOException {
+        loadProperties();
+        connect();
+
+    }
+
+
+    private void loadProperties() throws IOException {
+        FileInputStream in = new FileInputStream(CONFIG_FILE);
+        config.load(in);
+        in.close();
+    }
+
+
+    public boolean connect(){
+        try {
+            s.startConnection(config.get("controller-address").toString(), (int) config.get("controller-port"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+
 
 }
--- src/Client/src/mainWindow/Main.java
+++ src/Client/src/mainWindow/Main.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    ClientController cc;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,6 +15,9 @@ public class Main extends Application {
         primaryStage.setTitle("Watching Nemo");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+        cc = new ClientController();
+        cc.connect();
     }
 
 
