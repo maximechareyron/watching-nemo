e3466958ecd24b3c151ccced860c874aea0e74d0
Luca Sonet <lsonet001@enseirb-matmeca.fr>
2019-03-25 12:23:25 +0100

    prompt

---
+++ src/Client/src/mainWindow/Prompt.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Prompt {
+
+    public static void main(String[] args) throws IOException{
+    InputStreamReader converter = new InputStreamReader(System.in);
+    BufferedReader in = new BufferedReader(converter);
+    String cmd = in.readLine();
+
+    while((cmd = in.readLine()) != null) {
+
+    }
+  }
+}
