b57bfafb714942d10b0cfeb19a7dc7ae6a0d0b01
Guillaume André <gandre001@enseirb-matmeca.fr>
2019-04-17 11:02:50 +0200

    Correction d'une erreur de compilation

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 export CC=gcc
-export CFLAGS=-Wall -Wextra -std=c99 -g -O0
+export CFLAGS=-Wall -Wextra -std=gnu99 -g -O0
 PWD=$(shell pwd)
 export BUILD_DIR=$(PWD)/build
 
--- src/controller/mobility.c
+++ src/controller/mobility.c
@@ -3,10 +3,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+
 #include "mobility.h"
 #include "controller.h"
 #include "fish.h"
 #include "aquarium.h"
+
 
 struct size aquarium_size;
 
