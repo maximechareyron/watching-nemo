f106c4a16383a24d85df3dfe7c7d7949172f34aa
Luca Sonet <lsonet001@enseirb-matmeca.fr>
2019-05-13 14:05:56 +0200

    needed

--- src/Client/src/mainWindow/Operations.java
+++ src/Client/src/mainWindow/Operations.java
@@ -3,16 +3,14 @@ package mainWindow;
 public class Operations {
 
   public void status() {
-    
   }
 
   public void addFish(String fishName, String coordinate, String size, String mobility) {
-
   }
+
   public void delFish(String fishName) {
-
   }
-  public void startFish(String fishName) {
 
+  public void startFish(String fishName) {
   }
 }
