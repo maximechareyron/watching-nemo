6e301bb0beb6db67c8f296535e084a6b5ceb114c
Luca Sonet <lsonet001@enseirb-matmeca.fr>
2019-05-20 14:27:35 +0200

    opih

--- src/client/mainwindow/Operations.java
+++ src/client/mainwindow/Operations.java
@@ -8,48 +8,10 @@ class Operations {
 
   void status(SocketHandler s, PrintStream out) throws Exception {
     s.sendMessage("getFishes");
-    /*String rec = "";
-    try {
-      do {
-        rec = s.receiveMessage();
-      } while (rec.startsWith("pong"));
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    String[] recSplit = rec.split(" \\[");
-    if (!recSplit[0].equals("list")) {
-      out.println("No connected\n");
-    }
-    else {
-      out.println("OK : Connecté au contrôleur, " + (recSplit.length - 1) + " poissons trouvés\n");
-      //DrawFishes.draw(rec);
-      for (String fish : recSplit) {
-        if (!fish.equals("list")) {
-          out.println(fish.substring(0, fish.length() - 1) + "\n");
-        }
-      }
-    }*/
   }
 
   void addFish(String fishName, String coordinate, String size, String mobility, SocketHandler s, PrintStream out) throws Exception {
     s.sendMessage("addFish " + fishName + " at " + coordinate + "," + size + ", " + mobility);
-    /*String rec = "";
-    try {
-      do {
-        rec = s.receiveMessage();
-        // if (rec.startsWith("list")) {
-        //   DrawFishes.draw(rec);
-        // }
-      } while (rec.startsWith("list") || rec.startsWith("pong"));
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    if (rec.equals("OK")) {
-      out.println("OK\n");
-    }
-    else {
-      out.println(rec + " : modèle de mobilité non supporté\n");
-    }*/
   }
 
   void delFish(String fishName, SocketHandler s, PrintStream out) throws Exception {
@@ -58,9 +20,6 @@ class Operations {
     try {
       do {
         rec = s.receiveMessage();
-        // if (rec.startsWith("list")) {
-        //   DrawFishes.draw(rec);
-        // }
       } while (rec.startsWith("list") || rec.startsWith("pong"));
     } catch (IOException e) {
         e.printStackTrace();
@@ -75,22 +34,5 @@ class Operations {
 
   void startFish(String fishName, SocketHandler s, PrintStream out) throws Exception {
     s.sendMessage("startFish " + fishName);
-    /*String rec = "";
-    try {
-      do {
-        rec = s.receiveMessage();
-        // if (rec.startsWith("list")) {
-        //   DrawFishes.draw(rec);
-        // }
-      } while (rec.startsWith("list") || rec.startsWith("pong"));
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    if (rec.equals("OK")) {
-      out.println("OK\n");
-    }
-    else {
-      out.println(rec + " : Poisson inexistant\n");
-    }*/
   }
 }
